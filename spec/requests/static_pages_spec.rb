ccd0fa1e34a2f8b6c3aa37b79ddcee1f2f40493d
satanir <satanir@gmail.com>
2013-01-14 23:16:39 +0200

    Refactored tests and added routes testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,27 +2,51 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it {should have_selector('title', text: full_title(page_title))}
+    it {should have_selector('h1',    text: heading)}
+  end
+
   describe "Home page" do
     before {visit root_path}
-    it {should have_selector('title', text: full_title(''))}
-    it {should have_selector('h1', text: "Sample App")}
+    let(:heading) {"Sample App"}
+    let(:page_title) {''}
+
+    it_should_behave_like "all static pages"
+    it {should_not have_selector 'title', text: ' | Home'}
   end
 
   describe "Help page" do
     before {visit help_path}
-    it {should have_selector('title', text: full_title('Help'))}
-    it {should have_selector('h1', text: "Help")}
+    let(:heading) {"Help"}
+    let(:page_title) {'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before {visit about_path}
-    it {should have_selector('title', text: full_title('About'))}
-    it {should have_selector('h1', text: "About us")}
+    let(:heading) {"About us"}
+    let(:page_title) {'About'}
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before {visit contact_path}
-    it {should have_selector('h1', text: "Contact")}
-    it {should have_selector('title', text: full_title('Contact'))}
+    let(:heading) {"Contact"}
+    let(:page_title) {'Contact us'}
+    it_should_behave_like "all static pages"
+  end
+
+  it "Should have the right links" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact us')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
   end
 end
