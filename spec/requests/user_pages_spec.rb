b73b63d2bed05d2ec5cde4d6d35be948fc6030b0
satanir <satanir@gmail.com>
2013-01-27 21:14:06 +0200

    styling

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,18 +24,33 @@ describe "User pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect {click_button submit}.not_to change(User, :count)
-      end      
+      end
+
+      describe "after submission" do
+        before {click_button submit}
+        it {should have_selector('title', text: full_title('Signup'))}
+        it {should have_content 'error'}
+        it {should have_content 'Email is invalid'}
+      end
     end
 
     describe "with valid information" do
-      it "should create a new user" do
         before do
           fill_in "Name",         with: "Example user"
           fill_in "Email",        with: "exa@example.com"
           fill_in "Password",     with: "Foobar"
           fill_in "Confirmation", with: "Foobar"
         end
+      it "should create a new user" do
         expect {click_button submit}.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before {click_button submit}
+        let(:user) {User.find_by_email("exa@example.com")}
+
+        it {should have_selector 'title', text: user.name}
+        it {should have_selector 'div.alert.alert-success', text: "Welcome"}
       end
     end
   end
