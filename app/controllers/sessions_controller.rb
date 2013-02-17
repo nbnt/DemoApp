02b7517645d2ba5143fed6c5732fe3cc91143ace
satanir <satanir@gmail.com>
2013-02-17 20:11:59 +0200

    added authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:sessions][:email])
-    if(user && user.authenticate(params[:sessions][:password]))
+    user = User.find_by_email(params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      sign_in user
+      redirect_to user
     else
       flash.now[:error] = "Invalid email or password"
       render 'new'
@@ -13,6 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    
+    sign_out
+    redirect_to root_url
   end
 end
